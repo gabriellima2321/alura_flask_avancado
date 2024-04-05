b01d827f6a818c76ef0e828fa9c42148bb44721e
Gabriel Leite Lima <53106172+gabriellima2321@users.noreply.github.com>
2024-04-05 20:27:11 -0300

    Delete flaskProject directory

--- flaskProject/games.py
+++
@@ -1,9 +0,0 @@
-
-class Game:
-    def __init__(self,nome,categoria,console):
-        self._nome = nome.title()
-        self._categoria = categoria.title()
-        self._console = console.title()
-
-    def __str__(self):
-        return f'Nome do Jogo: {self._nome} | Categoria do Jogo: {self._categoria} | Console do Jogo: {self._console}'
